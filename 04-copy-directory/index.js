52c208668a0635d6d07a24dea468621f1233bcf1
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-05-24 00:57:50 +0200

    feat: accomplish 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const path = require('path');
+const fs = require('fs');
 
+const filePath = path.join(__dirname, 'files');
+const fileCopyPath = path.join(__dirname, 'files-copy');
+
+
+function copyDir () {
+
+  // delete directory
+  fs.rm(fileCopyPath, { recursive: true }, () => {
+    fs.mkdir(fileCopyPath, {recursive: true, force: true}, (err) => {
+      if(err) throw err;
+    } );
+
+    fs.readdir(filePath, (err, files) => {
+        
+      if(err) throw err;
+    
+      for (const file of files) {
+    
+        // copy each file to files-copy
+        fs.copyFile((path.join(filePath, file)), (path.join(fileCopyPath, file)), () => {
+          console.log(`file ${file} copied`);
+        });
+      }
+    });
+  });
+}
+
+copyDir();
