58c25ac4b84a6c3854cdf2f4e47774f2feae7e28
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-05-23 18:00:04 +0200

    feat: add greeting message, write to the text file and add event on ctrl+c

---
+++ 02-write-file/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const textFilePath = path.join(__dirname, 'destination.txt');
+
+// Создание потока записи в текстовый файл
+const output = fs.createWriteStream(textFilePath);
+
+// Вывод в консоль приветственного сообщения
+console.log('Enter your text');
+
+// Получение введенных данных и запись текста в файл
+stdin.on('data', (data) => {
+
+  let dataStrignified = data.toString();
+
+  console.log('data:', dataStrignified);
+
+  // Проверка на exit
+  if(dataStrignified === 'exit') {
+    // console.log('Process ended!');
+    process.exit();
+  }
+
+  output.write(data);
+  console.log('Enter one more text');
+});
+
+// Реализация прощального сообщения при остановке процесса
+process.on('exit', () => stdout.write('Process ended!'));
+process.on('SIGINT', () => {
+  process.exit();
+});
+
+
+
+
