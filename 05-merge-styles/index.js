c6868e27257f062c0da7ba79dd567757653c6555
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-05-26 21:21:19 +0200

    feat: add functionality to actualize bundle when changes the initial files

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,31 +4,30 @@ const fs = require('fs');
 const stylePath = path.join(__dirname, 'styles');
 const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
 
-fs.readdir(stylePath, {withFileTypes: true}, (err, files) => {
-  if (err) throw err;
-
-  
-  for (const file of files) {
-
-    const fileExt = path.extname(file.name.toString());
-    const filePath = path.join(stylePath, file.name);
-
-    if(file.isFile() && fileExt == '.css') {
-
-      fs.readFile(filePath, (err, data) => {
+fs.rm( bundlePath, { recursive: true }, () => {
+  console.log('file removed');
+    fs.readdir(stylePath, {withFileTypes: true}, (err, files) => {
         if (err) throw err;
-                
-        fs.appendFile(bundlePath, data.toString(), (err) => {
-          if(err) throw err;
-          console.log(`${file.name} added to bundle file`);
-        });
+      
+        
+        for (const file of files) {
+      
+          const fileExt = path.extname(file.name.toString());
+          const filePath = path.join(stylePath, file.name);
+      
+          if(file.isFile() && fileExt == '.css') {
+      
+            fs.readFile(filePath, (err, data) => {
+              if (err) throw err;
+                      
+              fs.appendFile(bundlePath, data.toString(), (err) => {
+                if(err) throw err;
+                console.log(`${file.name} added to bundle file`);
+              });
+            });
+          
+          }
+        }
       });
-    
-    }
-  }
-});
-
-
-
-
-
+}
+)
